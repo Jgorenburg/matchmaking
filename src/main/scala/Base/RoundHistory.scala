a423e608593df94ce4e7ee58d2c1cc237e4df3d8
Jgorenburg <jacobgorenburg@gmail.com>
2024-06-10 20:49:05 -0400

    parameterize roundNum field in RoundHistory

--- src/main/scala/Base/RoundHistory.scala
+++ src/main/scala/Base/RoundHistory.scala
@@ -2,8 +2,7 @@ package Base
 
 import scala.collection.mutable.ArrayBuffer
 
-class RoundHistory(roundNumParam: Int):
+class RoundHistory(val roundNum: Int):
   val matches = ArrayBuffer[MatchHistory]()
-  val roundNum = roundNumParam
 
   def addMatch(matchRecord: MatchHistory): Unit = matches += matchRecord
