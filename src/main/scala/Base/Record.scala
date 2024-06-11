4241a3a3788f10be329a0dd5414bc59c3de9a34d
Jgorenburg <jacobgorenburg@gmail.com>
2024-06-10 20:32:40 -0400

    remove unused defs in Record

--- src/main/scala/Base/Record.scala
+++ src/main/scala/Base/Record.scala
@@ -6,8 +6,6 @@ class Record:
   def updateRecord(won: Boolean) =
     games += 1
     if (won) wins += 1
-  def wonGame = updateRecord(true)
-  def lostGame = updateRecord(false)
   def getWinPercent: Float =
     if (games == 0)
       return 0.1
