339fdcae257390edcd27aba9d098f64b462bb548
Jgorenburg <jacobgorenburg@gmail.com>
2024-06-17 14:07:21 -0400

    fix getWinPercent bug

--- src/main/scala/Base/Record.scala
+++ src/main/scala/Base/Record.scala
@@ -1,12 +1,12 @@
 package Base
 
 class Record:
-  var wins = 0
-  var games = 0
+  var wins = 0.0f
+  var games = 0.0f
   def updateRecord(won: Boolean) =
     games += 1
     if (won) wins += 1
   def getWinPercent: Float =
     if (games == 0)
-      return 0.1
+      return 0.5
     return wins / games
