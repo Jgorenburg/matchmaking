15cc65c38895028a7de9e8cd4357b5789bd78c2e
Jgorenburg <jacobgorenburg@gmail.com>
2024-06-20 15:56:34 -0400

    Refactor Side

--- src/main/scala/Base/MatchTraits/Winner.scala
+++ src/main/scala/Base/MatchTraits/Winner.scala
@@ -3,17 +3,17 @@ package Base
 import scala.util.Random
 
 trait Winner {
-  def decideWinner(teams: (Champion, Champion)): Side
+  def decideWinner(teams: (Champion, Champion)): Side.Value
 }
 
 trait PureSkillWinner extends Winner {
-  def decideWinner(teams: (Champion, Champion)): Side =
+  def decideWinner(teams: (Champion, Champion)): Side.Value =
     val (blueChamp, redChamp) = teams
     if blueChamp.skill > redChamp.skill then Side.Blueside else Side.Redside
 }
 
 trait SkillAndVarianceWinner extends Winner {
-  def decideWinner(teams: (Champion, Champion)): Side =
+  def decideWinner(teams: (Champion, Champion)): Side.Value =
     val (blueChamp, redChamp) = teams
     if blueChamp.skill > Random.nextInt(blueChamp.skill + redChamp.skill)
     then Side.Blueside
